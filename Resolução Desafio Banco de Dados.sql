399af56c356d097aee7057316865c0ac4de161fa
LeoVale <151870779+leoRDvale@users.noreply.github.com>
2024-04-28 13:35:03 -0300

    Add files via upload

--- Resolução Desafio Banco de Dados.sql
+++ Resolução Desafio Banco de Dados.sql
@@ -1,46 +1,46 @@
 --Resolução Atividades, comandos e INNER JOIN 
 
---1
+--1 - Buscar o nome e ano dos filmes
 
 SELECT Nome, Ano FROM Filmes
 
---2
+--2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 
 SELECT Nome, Ano FROM Filmes ORDER BY Ano
 
---3
+--3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
 
 SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro' 
 
---4
+--4 - Buscar os filmes lançados em 1997
 
 SELECT * FROM Filmes WHERE Ano = 1997
 
---5
+--5 - Buscar os filmes lançados APÓS o ano 2000
 
 SELECT * FROM Filmes WHERE Ano > 2000 
 
---6
+--6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
 
 SELECT * FROM Filmes WHERE Duracao > 100 and Duracao < 150 ORDER BY Duracao 
 
---7
+--7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 
 SELECT Ano, COUNT(*) Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC
 
---8
+--8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
 
 SELECT  PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'M'
 
---9
+--9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
 
 SELECT PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome
 
---10
+--10 - Buscar o nome do filme e o gênero
 
 SELECT Filmes.Nome, Generos.Genero  FROM Filmes, FilmesGenero, Generos WHERE (Filmes.Id = FilmesGenero.IdFilme) AND (FilmesGenero.IdGenero = Generos.Id) 
 
---11 Necessário o uso do INNER JOIN para vincular tabelas
+--11 - Buscar o nome do filme e o gênero do tipo "Mistério". Necessário o uso do INNER JOIN para vincular tabelas
 
 SELECT 
      Filmes.Nome,
@@ -51,7 +51,7 @@ SELECT
   INNER JOIN Generos ON (FilmesGenero.IdGenero = Generos.Id ) 
   WHERE Genero = 'Mist�rio'
 
---12 Necessário o uso do INNER JOIN para vincular tabelas
+--12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel. Necessário o uso do INNER JOIN para vincular tabelas
 
 SELECT 
      Filmes.Nome,
